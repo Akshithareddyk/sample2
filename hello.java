d3e41d4c183a446f0af7d0f61e2df4bebe1e48bb
Akshithareddyk <akshithareddykandhala@gmail.com>
2025-07-31 14:53:15 +0530

    final commit

--- hello.java
+++ hello.java
@@ -3,5 +3,7 @@ public class hello
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Akshitha");
+		System.out.println("scm polling to monitor changes");
 	}
 }
